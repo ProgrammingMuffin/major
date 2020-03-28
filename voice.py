b9cd4d56ebca3983447546cd73fb1073cd14ae16
Deepak Ramachandran <42649107+ProgrammingMuffin@users.noreply.github.com>
2020-03-28 10:19:26 +0530

    add multi threading, refactoring

--- voice.py
+++ voice.py
@@ -1,5 +1,6 @@
 from gtts import gTTS as gtts
 from playsound import playsound
+import speech_recognition as sr
 import sys
 
 def playText(text):
@@ -10,13 +11,15 @@ def playText(text):
     
 def speechToText():
     r=sr.Recognizer()
+    r.energy_threshold = 250
+    r.non_speaking_duration = 0
+    r.pause_threshold = 0.5 #by default 1
     with sr.Microphone() as source:
         print('say something')
         audio=r.listen(source)
     try:
-        print('you said:\n'+r.recognize_google(audio))
+        text = r.recognize_google(audio)
+        return text
     except Exception:
-        print("an exception occured!: " + Exception)
-
-#test
-speechToText()
+        print("an exception occured!")
+        return None
